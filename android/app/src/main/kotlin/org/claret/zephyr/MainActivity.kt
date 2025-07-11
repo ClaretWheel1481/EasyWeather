b3961a3bf8b08f90e06954047a68162bd3bd071a
sheip9 <sheip9@outlook.com>
2025-07-11 11:47:07 +0800

    铲屎

--- android/app/src/main/kotlin/org/claret/zephyr/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package org.claret.zephyr
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/notifiers.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-final themeModeNotifier = ValueNotifier<ThemeMode>(ThemeMode.system);
-final tempUnitNotifier = ValueNotifier<String>('C');
-final dynamicColorEnabledNotifier = ValueNotifier<bool>(false);
-final ValueNotifier<int> localeIndexNotifier = ValueNotifier(0);
